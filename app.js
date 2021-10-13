672260899292f9fb756835ef183b52e408f7450d
vahidjabarouti <vahid.jabarouti@gmail.com>
2021-10-13 16:58:21 +0330

    adding morgan

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const fs = require('fs')
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
 
+//1) Middlewares
+app.use(morgan('dev'))
 //Adding req.body
 app.use(express.json())
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware')
+  next()
+})
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+})
+
+//Route Handlers
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime)
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours: tours
@@ -19,7 +36,7 @@ const getAllTours = (req, res) => {
 }
 
 const getTour = (req, res) => {
-  console.log(req.params)
+  //console.log(req.params)
   const id = req.params.id * 1;
 
   const tour = tours.find(el => el.id === id)
@@ -41,7 +58,6 @@ const getTour = (req, res) => {
 }
 
 const createTour = (req, res) => {
-  //console.log(req.body)
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({id: newId}, req.body)
 
@@ -57,16 +73,11 @@ const createTour = (req, res) => {
 }
 
 const updateTour = (req, res) => {
-  const id = req.params.id * 1;
-
-  const tour = tours.find(el => el.id === id)
-
-  //if(id > tours.length)
-  if(!tour) {
+  if(req.params.id * 1 > tours.length) {
     return res.status(404).json({
-      status: 'failed',
-      message: "Invalid ID"
-    })
+      status: 'fail',
+      message: 'Invalid ID'
+    });
   }
 
   res.status(200).json({
@@ -90,12 +101,6 @@ const deleteTour = (req, res) => {
     data: null
   })
 }
-
-// app.get('/api/v1/tours', getAllTours)
-// app.get('/api/v1/tours/:id', getTour)
-// app.post('/api/v1/tours', createTour)
-// app.patch('/api/v1/tours/:id', updateTour)
-// app.delete('/api/v1/tours/:id', deleteTour)
 
 app
   .route('/api/v1/tours')
