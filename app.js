3a209cf24c726c5f028215598b7bdfc79b3fbde7
vahidjabarouti <vahid.jabarouti@gmail.com>
2021-10-13 14:46:46 +0330

    adding post route for adding new tour

--- app.js
+++ app.js
@@ -3,16 +3,8 @@ const express = require('express')
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    msg: "hello and welcome to main page!",
-  })
-})
-
-app.post('/', (req, res) => {
-  res.status(404).send("You are not allowed to post on this page!")
-})
+//Adding req.body
+app.use(express.json())
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`))
 
@@ -23,6 +15,22 @@ app.get('/api/v1/tours', (req, res) => {
     data: {
       tours: tours
     }
+  })
+})
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body)
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({id: newId}, req.body)
+
+  tours.push(newTour)
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    res.status(201).json({
+      status: "success",
+      data: {
+        tour: newTour
+      }
+    })
   })
 })
 
