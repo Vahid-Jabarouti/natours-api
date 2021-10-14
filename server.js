d0285d6c3b42ba0083cf672a8dfcaa34fdae75e1
vahidjabarouti <vahid.jabarouti@gmail.com>
2021-10-14 19:30:44 +0330

    adding mongodb

--- server.js
+++ server.js
@@ -1,9 +1,45 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 dotenv.config({path: './config.env'})
 const app = require('./app')
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-console.log(process.env)
+mongoose
+  //.connect(process.env.DATABASE_LOCAL)
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection successful!')
+})
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price']
+  }
+})
+const Tour = mongoose.model('Tour', tourSchema)
+
+const testTour = new Tour({
+  name: 'The park camper',
+  rating: 4.5,
+  price: 927
+})
+
+testTour.save().then(doc => {
+  console.log(doc)
+}).catch(err => {
+  console.log('Error: ', err)
+})
 
 const port = process.env.PORT || 3000;
 
@@ -11,4 +47,5 @@ app.listen(port, () => {
   console.log(`App running on port ${port}` + 
   ' Press Ctrl-C to terminate...')
 })
+
 
