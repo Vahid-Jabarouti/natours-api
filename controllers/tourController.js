2a48f4fa9d756616805940b6c3de48fd42ece302
vahidjabarouti <vahid.jabarouti@gmail.com>
2021-10-14 19:37:44 +0330

    preparing for mvc

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,20 +1,16 @@
-const fs = require('fs')
+const Tour = require('./../models/tourModel')
 
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-)
+// exports.checkID = (req, res, next, val) => {
+//   console.log(`tour id is: ${val}`)
 
-exports.checkID = (req, res, next, val) => {
-  console.log(`tour id is: ${val}`)
-
-  if(req.params.id * 1 > tours.length) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID'
-    });
-  }
-  next()
-}
+//   if(req.params.id * 1 > tours.length) {
+//     return res.status(404).json({
+//       status: 'fail',
+//       message: 'Invalid ID'
+//     });
+//   }
+//   next()
+// }
 
 exports.checkBody = (req, res, next) => {
   if (!req.body.name || !req.body.price) {
@@ -32,10 +28,10 @@ exports.getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     requestedAt: req.requestTime,
-    results: tours.length,
-    data: {
-      tours: tours
-    }
+    // results: tours.length,
+    // data: {
+    //   tours: tours
+    // }
   })
 }
 
@@ -43,29 +39,20 @@ exports.getTour = (req, res) => {
   //console.log(req.params)
   const id = req.params.id * 1;
 
-  const tour = tours.find(el => el.id === id)
+  // const tour = tours.find(el => el.id === id)
 
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: tour
-    }
-  })
+  // res.status(200).json({
+  //   status: 'success',
+  //   data: {
+  //     tour: tour
+  //   }
+  // })
 }
 
 exports.createTour = (req, res) => {
-  const newId = tours[tours.length - 1].id + 1;
-  const newTour = Object.assign({id: newId}, req.body)
-
-  tours.push(newTour)
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
     res.status(201).json({
-      status: "success",
-      data: {
-        tour: newTour
-      }
+      status: "success"
     })
-  })
 }
 
 exports.updateTour = (req, res) => {
