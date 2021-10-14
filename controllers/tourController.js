29ce2067d2a6a7f4bfd9ebe5c1b52d430556ae12
vahidjabarouti <vahid.jabarouti@gmail.com>
2021-10-15 00:17:54 +0330

    adding import-data script for importing data from JSON file

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -83,7 +83,7 @@ exports.createTour = async (req, res) => {
   } catch(err) {
     res.status(400).json({
       status: 'failed!',
-      message: 'Invalid data sent!'
+      message: err
     })
   }
 }
--- models/tourModel.js
+++ models/tourModel.js
@@ -4,7 +4,24 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique: true
+    unique: true,
+    trim: true
+  },
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
   },
   rating: {
     type: Number,
@@ -13,7 +30,27 @@ const tourSchema = new mongoose.Schema({
   price: {
     type: Number,
     required: [true, 'A tour must have a price']
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, 'A tour must have a description'],
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image']
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 })
 const Tour = mongoose.model('Tour', tourSchema)
 
