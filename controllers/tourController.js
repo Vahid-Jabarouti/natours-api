63db893a002b5441638750fce8cba87ee05a975e
vahidjabarouti <vahid.jabarouti@gmail.com>
2021-10-15 23:41:42 +0330

    adding querystrings and deleting excluded ones

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -25,31 +25,42 @@ const Tour = require('./../models/tourModel')
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = {...req.query}
-    const excludedFields = ['page', 'sort', 'limit', 'fields']
-    excludedFields.forEach(el => delete queryObj[el])
-    console.log(req.query)
-    const query = Tour.find(queryObj)
-  
-    // const tour = await Tour.find()
+
+    //Build query
+    //1) Filtering 
+    const queryObj = { ...req.query };
+
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+
+    excludedFields.forEach(el => delete queryObj[el]);
+
+
+    //2)Advanced filtering
+    let queryStr = JSON.stringify(queryObj);
+
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+    //console.log(JSON.parse(queryStr)
+    const tours = await Tour.find(JSON.parse(queryStr))
+    // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy')
 
     //Execute query
-    const tours = await query
+    //const tours = await query;
 
     //console.log(req.requestTime)
     res.status(200).json({
       status: 'success',
+      results: tours.length,
       data: {
         tours: tours
       }
     })
   } catch (err) {
     res.status(404).json({
-      status: 'failed!',
+      status: 'failed!!!!',
       message: err
     })
   }
